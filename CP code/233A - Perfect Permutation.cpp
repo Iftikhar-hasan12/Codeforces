e9b52223256c1499d6df26b49e9d30b374e44881
Iftikhar-hasan12 <iftikharhasan04@gmail.com>
2025-03-28 21:13:16 +0600

    Create 	233A - Perfect Permutation.cpp

---
+++ CP code/233A - Perfect Permutation.cpp
@@ -0,0 +1,28 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin>>n;
+
+    if(n<=1 || (n%2)!=0)
+    {
+        cout<<-1<<endl;
+    }
+    else
+    {
+        for(int i= n; i>=1 ; i--)
+        {
+
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+
+}
