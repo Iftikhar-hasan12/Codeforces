e3fec050081460ca446857016cee34143f3c331f
Iftikhar-hasan12 <iftikharhasan04@gmail.com>
2025-04-08 10:06:20 +0600

    Create 	124A - The number of positions.cpp

---
+++ CP code/124A - The number of positions.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, a, b;
+    cin>>n>>a>>b;
+
+   int tem  = n-a;
+      if(tem>b)
+      {
+
+          tem = b +1;
+
+      }
+
+
+    cout<<tem<<endl;
+
+
+}
