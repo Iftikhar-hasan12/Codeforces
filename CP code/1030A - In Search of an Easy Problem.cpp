b2781e671327fbfa445867713a624b26d954505c
Iftikhar-hasan12 <iftikharhasan04@gmail.com>
2025-04-28 05:37:59 +0600

    Create 1030A - In Search of an Easy Problem.cpp

---
+++ CP code/1030A - In Search of an Easy Problem.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+    int n;
+    cin>>n;
+    int flag=0;
+    while(n--)
+    {
+        int val;
+        cin>>val;
+        if(val==1)
+        {
+            flag=1;
+        }
+
+    }
+
+    if(flag==1)
+    {
+
+        cout<<"HARD"<<endl;
+    }
+    else
+    {
+        cout<<"EASY"<<endl;
+
+    }
+
+
+}
