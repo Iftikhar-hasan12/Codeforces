f13f4c7ed8084a477cc53a36963f0886ac7e2adc
Iftikhar-hasan12 <iftikharhasan04@gmail.com>
2025-04-28 06:47:58 +0600

    Create 486A - Calculating Function.cpp

---
+++ CP code/486A - Calculating Function.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+   long long int n;
+   long long int sum=0;
+    cin>>n;
+
+    if(n%2==0)
+    {
+        cout<<n/2<<endl;
+    }else
+    {
+
+        cout<< -((n/2) + 1)<<endl;
+    }
+
+
+}
