bc3d919e6a9aff71f590f1cf6dbb96ebc28c2881
Iftikhar-hasan12 <iftikharhasan04@gmail.com>
2025-05-01 15:02:13 +0600

    Create 1692A - Marathon.cpp

---
+++ CP code/1692A - Marathon.cpp
@@ -0,0 +1,33 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+    int t;
+    cin>>t;
+    while(t--){
+    int a, b, c,d;
+    cin>>a>>b>>c>>d;
+    int i=0;
+    if(a<b)
+    {
+
+        i++;
+    }
+    if(a<c)
+    {
+
+        i++;
+    }
+    if(a<d)
+    {
+
+        i++;
+    }
+    cout<<i<<endl;
+
+    }
+
+
+}
