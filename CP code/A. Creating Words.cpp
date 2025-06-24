09d519e9a5071735f67d2111a9905a511bef15eb
Iftikhar-hasan12 <iftikharhasan04@gmail.com>
2025-06-24 20:49:02 +0600

    Create A. Creating Words.cpp

---
+++ CP code/A. Creating Words.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+    string s,s2;
+    cin>>s>>s2;
+  
+     swap(s[0],s2[0]);
+     cout<<s<<" "<<s2<<endl;
+ 
+}
+}
