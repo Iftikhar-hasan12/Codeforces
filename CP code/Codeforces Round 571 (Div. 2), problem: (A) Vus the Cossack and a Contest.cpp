ee0ca8183d81ff4f0b8911812445ed8f1c911852
Iftikhar Hasan <iftikharhasan04@gmail.com>
2025-07-19 09:03:07 +0600

    Create Codeforces Round 571 (Div. 2), problem: (A) Vus the Cossack and a Contest.cpp

---
+++ CP code/Codeforces Round 571 (Div. 2), problem: (A) Vus the Cossack and a Contest.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+    
+    int n, m, k;
+    cin>>n>>m>>k;
+    if(k>=n && m>=n)
+    {
+        cout<<"YES"<<endl;
+    }else
+    {
+        
+       cout<<"NO"<<endl; 
+    }
+}
