b6084800684617c1020773a0514509ee0d7af286
Iftikhar Hasan <iftikharhasan04@gmail.com>
2025-07-19 08:59:28 +0600

    Create Codeforces Round 839 (Div. 3), problem: (A) A+B?.cpp

---
+++ CP code/Codeforces Round 839 (Div. 3), problem: (A) A+B?.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve();
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){solve();}
+}
+void solve()
+{
+
+    string s;
+    cin>>s;
+    int a = s[0] -'0';
+    int b = s[2] -'0';
+cout<<a+b<<endl;
+
+
+}
