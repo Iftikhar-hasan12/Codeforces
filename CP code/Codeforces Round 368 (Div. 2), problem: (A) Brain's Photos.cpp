9ed7b9e150816762f714b6f6d1a6439cfc149a4c
Iftikhar Hasan <iftikharhasan04@gmail.com>
2025-07-19 09:00:06 +0600

    Create Codeforces Round 368 (Div. 2), problem: (A) Brain's Photos.cpp

---
+++ CP code/Codeforces Round 368 (Div. 2), problem: (A) Brain's Photos.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n, m;
+    cin>>n>>m;
+    char mat[n][m];
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            cin>>mat[i][j];
+        }
+    }
+
+int flag=0;
+     for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            if(mat[i][j]=='C' ||mat[i][j]=='M'||mat[i][j]=='Y' )
+                flag=1;
+        }
+
+    }
+
+if(flag==0)
+{
+    cout<<"#Black&White"<<endl;
+}else
+{
+
+     cout<<"#Color"<<endl;
+}
+
+
+}
